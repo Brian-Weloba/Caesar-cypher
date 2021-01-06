a898e9a982e32cb093ac4d7864fbe71a52c5b80d
Brian <bweloba@gmail.com>
2021-01-06 19:27:23 +0300

    Trim console input

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,7 +22,7 @@ public class App {
             System.out.println("Would you like to: \n 1. ENCODE \n 2. DECODE \n 3. EXIT");
             int response = 0;
             try {
-                response = Integer.parseInt(con.readLine());
+                response = Integer.parseInt(con.readLine().trim());
             } catch (NumberFormatException e) {
                 System.out.println(" ");
             }
@@ -31,7 +31,7 @@ public class App {
                 System.out.println("Input text to encode: ");
                 String inputText = con.readLine().toLowerCase();
                 System.out.println("Enter key: ");
-                int key = Integer.parseInt(con.readLine());
+                int key = Integer.parseInt(con.readLine().trim());
                 Cypher cypher = new Cypher(key, inputText);
                 if (cypher.isInputValid(inputText) && cypher.isKeyValid(key)) {
                     System.out.println("The encoded text is: \n"+ cypher.encode());
@@ -41,7 +41,7 @@ public class App {
                 System.out.println("Input text to decode: ");
                 String inputText = con.readLine().toLowerCase();
                 System.out.println("Enter key: ");
-                int key = Integer.parseInt(con.readLine());
+                int key = Integer.parseInt(con.readLine().trim());
                 Cypher cypher = new Cypher(key, inputText);
                 if (cypher.isInputValid(inputText) && cypher.isKeyValid(key)) {
                     System.out.println("The decoded text is: \n"+ cypher.decode());
