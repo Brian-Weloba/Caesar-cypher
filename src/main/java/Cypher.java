fcced9db8011b0d183eea4947f922635244946c7
Brian <bweloba@gmail.com>
2021-01-06 14:25:30 +0300

    Cypher code and test fies

---
+++ src/main/java/Cypher.java
@@ -0,0 +1,76 @@
+public class Cypher {
+    private int mKey;
+    private String mInput;
+
+    public int getmKey() {
+        return mKey;
+    }
+
+    public String getmInput() {
+        return mInput;
+    }
+
+    public Cypher(int mKey, String mInput) {
+        this.mKey = mKey;
+        this.mInput = mInput;
+    }
+
+    public Cypher() {
+    }
+
+    public boolean isInputValid(String mInput) {
+        char[] inputArray = mInput.toCharArray();
+
+        for (char c : inputArray) {
+            if (!Character.isLetter(c) && c != ' ') {
+                System.out.println("Input is not Valid");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isKeyValid(int key) {
+        return key >= 1 && key < 26;
+    }
+
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private char[] alphabetArray = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+    String encode() {
+        char[] inputArray = mInput.toCharArray();
+        StringBuilder encoded = new StringBuilder();
+
+        for (char c : inputArray) {
+            if (c == ' ') {
+                encoded.append(' ');
+            }
+            for (char letter : alphabetArray) {
+                if (c == letter) {
+                    encoded.append(alphabetArray[(alphabet.indexOf(c) + mKey) % 26]);
+                }
+            }
+        }
+
+        return encoded.toString();
+    }
+
+    String decode(){
+        char[] inputArray = mInput.toCharArray();
+        StringBuilder decoded = new StringBuilder();
+
+        for (char c : inputArray) {
+            if (c == ' ') {
+                decoded.append(' ');
+            }
+            for (char letter : alphabetArray) {
+                if (c == letter) {
+                    decoded.append(alphabetArray[alphabet.indexOf(c) - (mKey % 26)]);
+                }
+            }
+        }
+
+        return decoded.toString();
+    }
+}
